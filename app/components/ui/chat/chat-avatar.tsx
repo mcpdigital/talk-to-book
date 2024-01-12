01e82995381c7a9ba027a6e0fd16f6200e67112f
mcpdigital <internet@mcpdigital.com>
2024-01-12 17:17:35 -0300

    new docs

--- app/components/ui/chat/chat-avatar.tsx
+++ app/components/ui/chat/chat-avatar.tsx
@@ -11,11 +11,11 @@ export default function ChatAvatar({ role }: { role: string }) {
   }
 
   return (
-    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
+    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center    text-white shadow">
       <Image
-        className="rounded-md"
-        src="/llama.png"
-        alt="Llama Logo"
+        className=""
+        src="/cover.png"
+        alt="Book cover"
         width={24}
         height={24}
         priority
